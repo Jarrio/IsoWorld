deb4e44473ee97ead984264ec608c34a20bcbe05
Jarrio <Jarrio@users.noreply.github.com>
2016-11-03 16:46:32 +0000

    Missing folders
    For some reason, git isn't pushing this folder to the repo

---
+++ source/system/debug/CustomCommands.hx
@@ -0,0 +1,31 @@
+package system.debug;
+
+import flixel.FlxG;
+import system.entities.IsoSprite;
+import state.MenuState;
+
+class CustomCommands {
+    private var state:MenuState;
+    public function new(state:MenuState) {
+        this.state = state;
+    }
+
+    public function sprite(sprite:IsoSprite) {
+        var iso = FlxG.debugger.track(sprite, sprite.entity + " sprite");
+        iso.reposition(0, iso.height + iso.height);
+
+        var body_height = iso.y + iso.height;
+        FlxG.debugger.track(sprite.iso_bounds, sprite.entity + " body").reposition(0, body_height);
+    }
+
+    public function group(member:Int):IsoSprite {
+        var sprite = new IsoSprite();
+        if (this.state.group.members[member] != null) {
+            sprite = this.state.group.members[member];
+        }
+
+        return sprite;
+    }    
+
+
+}
---
+++ source/system/debug/TrackerProfiles.hx
@@ -0,0 +1,29 @@
+package system.debug;
+
+import system.entities.physics.Body;
+import system.entities.physics.Cube;
+import system.entities.IsoSprite;
+import flixel.FlxSprite;
+import flixel.FlxBasic;
+import flixel.FlxObject;
+import flixel.system.debug.watch.Tracker.TrackerProfile;
+class TrackerProfiles {
+
+    public var profiles:Array<TrackerProfile>;
+
+    public function new() {
+        profiles = new Array<TrackerProfile>();
+        IsoSpriteProfile();        
+        BodyProfile();
+    }
+
+    private function IsoSpriteProfile() {
+        var profile = new TrackerProfile(IsoSprite, ["iso_depth", "x", "y", "z", "iso_position"]);
+        profiles.push(profile);
+    }
+
+    private function BodyProfile() {
+        var profile = new TrackerProfile(Body, ["position","velocity", "current_overlap", "overlap_y", "overlap_x", "facing"]);
+        profiles.push(profile);
+    }
+}
