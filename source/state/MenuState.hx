4669b7be78a40c379ed2bf1f25e40927de2462b6
j <Jarrio@users.noreply.github.com>
2023-01-28 12:18:12 +0000

    Get this sheeeet compiling

--- source/state/MenuState.hx
+++ source/state/MenuState.hx
@@ -1,4 +1,3 @@
-
 package state;
 
 import system.world.Chunk;
@@ -7,29 +6,21 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
-
 import flixel.util.FlxSort;
 import system.entities.IsoSprite;
 import system.world.Depth;
 import flixel.FlxCamera;
 import flixel.util.FlxColor;
-
 import system.world.Generate;
 import system.entities.Player;
 import system.debug.TrackerProfiles;
 import system.debug.CustomCommands;
 import system.entities.Block;
-
 import system.utility.ThreadPool;
 import flixel.ui.FlxButton;
-
 import flixel.addons.text.FlxTextField;
-// import flixel.addons.ui.FlxInputText;
-
 
 using flixel.util.FlxArrayUtil;
-
-
 
 import system.world.World;
 
@@ -37,10 +28,10 @@ class MenuState extends FlxState {
 	public var depth:Depth;
 	public var generate:Generate;
 
-	public var group:FlxTypedSpriteGroup<IsoSprite> = new FlxTypedSpriteGroup<IsoSprite>();
+	public var group:FlxTypedSpriteGroup<FlxTypedSpriteGroup<IsoSprite>> = new FlxTypedSpriteGroup<FlxTypedSpriteGroup<IsoSprite>>();
 	public var unloaded_chunks:Array<Chunk> = new Array<Chunk>();
 	public var text:FlxText;
-	
+
 	public var game:World;
 
 	public var player:Player;
@@ -54,9 +45,9 @@ class MenuState extends FlxState {
 	public var label_y:FlxTextField;
 	public var label_z:FlxTextField;
 
-	// public var text_x:FlxInputText;
-	// public var text_y:FlxInputText;
-	// public var text_z:FlxInputText;
+	public var text_x:FlxTextField;
+	public var text_y:FlxTextField;
+	public var text_z:FlxTextField;
 
 	public var render_distance:Int = 3;
 
@@ -66,8 +57,8 @@ class MenuState extends FlxState {
 
 	public var blocks_btn:FlxButton;
 
-	override public function create():Void {		
-		super.create();		
+	override public function create():Void {
+		super.create();
 		this.thread_pool = new ThreadPool(8);
 		game = new World();
 		#if next
@@ -76,37 +67,35 @@ class MenuState extends FlxState {
 		FlxG.autoPause = false;
 		FlxG.debugger.visible = true;
 		FlxG.stage.window.focus();
-		//FlxG.sound.play("Test");
+		// FlxG.sound.play("Test");
 		#end
-		
+
 		this.bgColor = FlxColor.WHITE;
 
-		
 		depth = new Depth();
 		generate = new Generate();
-		
 
 		player = new Player(0, 0, -1, null, this);
-		
-		// label_x = new FlxTextField(0, 0, 32, "x: ", 26);		
-		// label_x.color = FlxColor.BLACK;
 
-		// label_y = new FlxTextField(0, 0, 32, "y: ", 26);
-		// label_y.color = FlxColor.BLACK;
+		 label_x = new FlxTextField(0, 0, 32, "x: ", 26);
+		 label_x.color = FlxColor.BLACK;
 
-		// label_z = new FlxTextField(0, 0, 32, "z: ", 26);
-		// label_z.color = FlxColor.BLACK;
+		 label_y = new FlxTextField(0, 0, 32, "y: ", 26);
+		 label_y.color = FlxColor.BLACK;
+
+		 label_z = new FlxTextField(0, 0, 32, "z: ", 26);
+		 label_z.color = FlxColor.BLACK;
 
 		// text_x = new FlxInputText(0, 0, 100, Std.string(this.blocks_x), 26, FlxColor.BLACK, FlxColor.TRANSPARENT);
 		// text_y = new FlxInputText(0, 0, 100, Std.string(this.blocks_y), 26, FlxColor.BLACK, FlxColor.TRANSPARENT);
 		// text_z = new FlxInputText(0, 0, 100, Std.string(this.blocks_z), 26, FlxColor.BLACK, FlxColor.TRANSPARENT);
-		
+
 		// blocks_btn = new FlxButton(0, 0, "Reset", this.reset_blocks);
 
 		// player.visible = false;
-// 
+		//
 
-		this.camera.follow(player, FlxCameraFollowStyle.LOCKON);		
+		this.camera.follow(player, FlxCameraFollowStyle.LOCKON);
 
 		// for (i in 0...generate.members.length) {
 		// 	var member = generate.members[i];
@@ -116,28 +105,28 @@ class MenuState extends FlxState {
 		// 	group.add(member);
 		// }
 
-		// for (i in 0...generate.available_chunks.length) {
-		// 	var chunk = this.generate.available_chunks[i];
-		// 	chunk.ID = i;
-		// 	this.group.add(chunk);			
-		// }
-
-		for (y in 0...this.blocks_y) {
-			for (x in 0...this.blocks_x) {
-				for (z in 0...blocks_z) {
-					var member = new Block(x, y,z, AssetPaths.new_water_cube__png);
-					member.world = this;
-					group.add(member);
-				}
-			}
+		for (i in 0...generate.available_chunks.length) {
+			var chunk = this.generate.available_chunks[i];
+			chunk.ID = i;
+			this.group.add(chunk);
 		}
 
-		group.members.add(player);
-		add(group);		
+		// for (y in 0...this.blocks_y) {
+		// 	for (x in 0...this.blocks_x) {
+		// 		for (z in 0...blocks_z) {
+		// 			var member = new Block(x, y,z, AssetPaths.new_water_cube__png);
+		// 			member.world = this;
+		// 			group.add(member);
+		// 		}
+		// 	}
+		// }
 
-		// add(label_x);
-		// add(label_y);
-		// add(label_z);
+		group.members[0].add(player);
+		add(group);
+
+		add(label_x);
+		add(label_y);
+		add(label_z);
 
 		// add(text_x);
 		// add(text_y);
@@ -146,16 +135,14 @@ class MenuState extends FlxState {
 		// add(blocks_btn);
 		camera.zoom = 1;
 
-		
 		this.init();
-//		FlxG.cameras.add()
+		//		FlxG.cameras.add()
 	}
 
 	// public function reset_blocks() {
 	// 	this.blocks_x = Std.parseInt(this.text_x.text);
 	// 	this.blocks_y = Std.parseIntv(this.text_y.text);
 	// 	this.blocks_z = Std.parseInt(this.text_z.text);
-
 	// 	this.group.destroy();
 	// 	group = new FlxTypedSpriteGroup<IsoSprite>();
 	// 	for (y in 0...this.blocks_y) {
@@ -171,12 +158,10 @@ class MenuState extends FlxState {
 	// 	player.visible = false;
 	// 	group.add(player);
 	// 	add(group);
-
-	// 	this.camera.follow(player, FlxCameraFollowStyle.LOCKON);		
+	// 	this.camera.follow(player, FlxCameraFollowStyle.LOCKON);
 	// }
-
 	public var index_behind:Int = 0;
-	
+
 	public var debug_length:Int = 0;
 	public var show_debug:Bool = false;
 
@@ -187,28 +172,25 @@ class MenuState extends FlxState {
 		if (this.update_phase) {
 			this.running = true;
 			this.thread_pool.addTask(sort_members(x), null, this.thread_back(x));
-			
 		}
 		return null;
 	}
- 
+
 	public function sort_members(x:Dynamic):Dynamic {
 		if (this.update_phase) {
 			for (i in this.group) {
-				var chunk = cast(i, Chunk); 
-				if (chunk.chunk_x > (player.chunk_x + this.render_distance / 2) &&
-					chunk.chunk_x > (player.chunk_x - this.render_distance / 2) && 
-					chunk.chunk_y > (player.chunk_y + this.render_distance / 2) &&
-					chunk.chunk_y > (player.chunk_y - this.render_distance / 2)) {
-						i.kill();
-						// this.unloaded_chunks.push(i);
-						// this.group.remove(i, true);
+				var chunk = cast(i, Chunk);
+				if (chunk.chunk_x > (player.chunk_x + this.render_distance / 2)
+					&& chunk.chunk_x > (player.chunk_x - this.render_distance / 2)
+					&& chunk.chunk_y > (player.chunk_y + this.render_distance / 2)
+					&& chunk.chunk_y > (player.chunk_y - this.render_distance / 2)) {
+					i.kill();
+					// this.unloaded_chunks.push(i);
+					// this.group.remove(i, true);
 				} else {
 					i.revive();
 				}
 			}
-
-			
 
 			var current_x_chunk = Math.ceil(player.chunk_x - (this.generate.chunk_dimensions - 1) / 2);
 			var max_x_chunk = Math.ceil(player.chunk_x + (this.generate.chunk_dimensions - 1) / 2);
@@ -216,26 +198,26 @@ class MenuState extends FlxState {
 			var current_y_chunk = Math.ceil(player.chunk_y - (this.generate.chunk_dimensions - 1) / 2);
 			var max_y_chunk = Math.ceil(player.chunk_y + (this.generate.chunk_dimensions + 1) / 2);
 
-
-			for (i in 0...group.members.length) {				
+			for (i in 0...group.members.length) {
 				var chunk = group.members[i];
-				if (!chunk.alive) break;
+				if (!chunk.alive)
+					break;
 				for (j in 0...chunk.members.length) {
 					var a = chunk.members[j];
 					index_behind = 0;
-					
+
 					for (k in 0...chunk.members.length) {
 						if (j != k) {
 							var b = chunk.members[k];
-							if(depth.find_overlaps(a.iso_bounds.a_comparison, b.iso_bounds.b_comparison)) {
-								a.iso_sprites_behind[index_behind++] = b;						
+							if (depth.find_overlaps(a.iso_bounds.a_comparison, b.iso_bounds.b_comparison)) {
+								a.iso_sprites_behind[index_behind++] = b;
 							}
 						}
 					}
 					a.iso_visited = 0;
 				}
-			}		
-			
+			}
+
 			depth.sort_depth = 0;
 			for (i in 0...group.members.length) {
 				var chunk = group.members[i];
@@ -243,51 +225,55 @@ class MenuState extends FlxState {
 					depth.visit_node(chunk.members[j]);
 				}
 			}
-			
+
 			for (i in 0...group.members.length) {
 				var chunk = group.members[i];
 				chunk.sort(SortBy3d, FlxSort.DESCENDING);
 			}
-			
+
 			this.update_phase = true;
 			this.running = true;
 		}
 		return null;
 	}
+
 	public var first_run:Bool = true;
 
 	public var speed:Float = 30;
 
 	public var tab_index:Int = -1;
 	public var tab_active:Bool = false;
-	override public function update(elapsed:Float):Void	{	
-		// for (i in 0...group.members.length) {
-		// 	var chunk = group.members[i];
-		// 	if (!chunk.alive) break;
-		// 	for (j in 0...chunk..length) {
-		// 		chunk.members[j].iso_bounds.PreUpdate();	
-		// 	}
-		// }
 
-		// for (i in 0...group.members.length) {
-		// 	var chunk = group.members[i];
-		// 	if (!chunk.alive) break;
-		// 	for (j in 0...chunk.members.length) {
-		// 		this.depth.update_bounding_cube(chunk.members[j]);
-		// 	}
-		// }
-		
-		this.game.Collide(this.player, this.group.members); 
-		super.update(elapsed);	
+	override public function update(elapsed:Float):Void {
+		for (i in 0...group.members.length) {
+			var chunk = group.members[i];
+			if (!chunk.alive)
+				break;
+			for (j in 0...chunk.members.length) {
+				chunk.members[j].iso_bounds.PreUpdate();
+			}
+		}
 
-		// label_x.y = (this.player.y - 290);
-		// label_x.x = (this.player.x - 600);
+		for (i in 0...group.members.length) {
+			var chunk = group.members[i];
+			if (!chunk.alive)
+				break;
+			for (j in 0...chunk.members.length) {
+				this.depth.update_bounding_cube(chunk.members[j]);
+			}
+		}
 
-		// label_y.y = (this.player.y - 260);
-		// label_y.x = (this.player.x - 600);
+		this.game.Collide(this.player, this.group.members);
+		super.update(elapsed);
 
-		// label_z.y = (this.player.y - 230);
-		// label_z.x = (this.player.x - 600);
+		label_x.y = (this.player.y - 290);
+		label_x.x = (this.player.x - 600);
+
+		label_y.y = (this.player.y - 260);
+		label_y.x = (this.player.x - 600);
+
+		label_z.y = (this.player.y - 230);
+		label_z.x = (this.player.x - 600);
 
 		// text_x.y = (this.player.y - 290);
 		// text_x.x = (this.player.x - 600) + 40;
@@ -296,8 +282,8 @@ class MenuState extends FlxState {
 		// text_y.x = (this.player.x - 600) + 40;
 
 		// text_z.y = (this.player.y - 230);
-		// text_z.x = (this.player.x - 600) + 40;		
-		
+		// text_z.x = (this.player.x - 600) + 40;
+
 		// blocks_btn.y = 120;
 		// blocks_btn.x = 16;
 
@@ -307,10 +293,9 @@ class MenuState extends FlxState {
 		// 	this.thread_pool.addTask(sort_members(x), null, this.thread_back(x));
 		// 	this.first_run = false;
 		// }
-		
 
 		this.sort_members(null);
-		
+
 		if (FlxG.keys.justPressed.TAB) {
 			if (this.tab_index > -1 && this.tab_index < 2) {
 				this.tab_index++;
@@ -318,55 +303,53 @@ class MenuState extends FlxState {
 				this.tab_index = 0;
 			}
 		}
-		
+
 		// switch(tab_index) {
 		// 	case -1:
 		// 		this.text_x.hasFocus = false;
 		// 		this.text_y.hasFocus = false;
 		// 		this.text_z.hasFocus = false;
-		// 	case 0: 
+		// 	case 0:
 		// 		this.text_x.hasFocus = true;
 		// 		this.text_y.hasFocus = false;
 		// 		this.text_z.hasFocus = false;
-		// 	case 1: 
+		// 	case 1:
 		// 		this.text_x.hasFocus = false;
 		// 		this.text_y.hasFocus = true;
 		// 		this.text_z.hasFocus = false;
 
-		// 	case 2: 
+		// 	case 2:
 		// 		this.text_x.hasFocus = false;
 		// 		this.text_y.hasFocus = false;
 		// 		this.text_z.hasFocus = true;
-								
-				
+
 		// }
 
 		// if (FlxG.keys.justPressed.ENTER) {
 		// 	if (this.text_x.hasFocus || this.text_y.hasFocus || this.text_z.hasFocus) {
 		// 		// this.reset_blocks();
-		// 		this.tab_index = -1;		
+		// 		this.tab_index = -1;
 		// 	}
 		// }
-		
+
 		// FlxG.watch.addQuick("show debug", show_debug);
 
-		if(FlxG.keys.pressed.U) {
+		if (FlxG.keys.pressed.U) {
 			FlxG.camera.zoom -= 0.1;
-		} else if(FlxG.keys.pressed.O) {
+		} else if (FlxG.keys.pressed.O) {
 			FlxG.camera.zoom += 0.1;
 		}
 
-		
-		
-		// for (i in 0...group.members.length) {
-		// 	var chunk = group.members[i];
-		// 	if (!chunk.alive) break;
-		// 	for (j in 0...chunk.members.length) {
-		// 		chunk.members[j].iso_bounds.PostUpdate();	
-		// 	}
-		// }
+		for (i in 0...group.members.length) {
+			var chunk = group.members[i];
+			if (!chunk.alive)
+				break;
+			for (j in 0...chunk.members.length) {
+				chunk.members[j].iso_bounds.PostUpdate();
+			}
+		}
 
-		//FlxG.watch.addQuick("Intersects", this.game.intersects(player.iso_bounds, group.members[0].iso_bounds));
+		// FlxG.watch.addQuick("Intersects", this.game.intersects(player.iso_bounds, group.members[0].iso_bounds));
 		// FlxG.watch.addQuick("overlap", this.world.overlap);
 		// FlxG.watch.addQuick("max overlap", this.world.max_overlap);
 		FlxG.watch.addQuick("Player Chunk:", 'x: ${Math.fceil(player.iso_x / 3)} y: ${Math.fceil(player.iso_y / 3)}');
@@ -385,17 +368,15 @@ class MenuState extends FlxState {
 		}
 	}
 
-
-
 	private function SortBy3d(order:Int, a:IsoSprite, b:IsoSprite):Int {
-		//return FlxSort.byValues(order, b.iso_depth, a.iso_depth);
+		// return FlxSort.byValues(order, b.iso_depth, a.iso_depth);
 		if (a.iso_depth > b.iso_depth) {
 			return 1;
 		} else if (a.iso_depth < b.iso_depth) {
 			return -1;
 		}
 		return 0;
-	}	
+	}
 
 	public function init() {
 		// #if debug
@@ -415,9 +396,9 @@ class MenuState extends FlxState {
 		// var window = FlxG.debugger.track(player, "Player sprite");
 		// window.reposition(0, 0);
 		// FlxG.debugger.track(player.iso_bounds, "Player body").reposition(0, window.height);
-		
+
 		// FlxG.debugger.track(group.members[0], "Block sprite").reposition(window.width, 0);
 		// FlxG.debugger.track(group.members[0].iso_bounds, "Block body").reposition(window.width, window.height);
-		// #end		
+		// #end
 	}
 }
